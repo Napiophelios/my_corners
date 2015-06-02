94b2be3405e2d89abbdb523daf55e5be6aa7c030
Napiophelios <napiophelios@gmail.com>
2015-06-02 06:43:24 -0400

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,19 @@
+--[[
+'New Corners' mod [new_corners]
+by Napiophelios
+License: WTFPL
+
+This is an experiment to reproduce 'Mycorners' mod
+by Don Bateman but without adding textures.
+
+for more advanced examples of how to use texture modifiers,
+specifically concerning the [combine texture modifier;
+check out 'Connected Chests' mod by HybridDog
+It is very good place to study from.
+--]]
+
+dofile(minetest.get_modpath("new_corners").."/sandstone.lua")
+dofile(minetest.get_modpath("new_corners").."/sandstone_brick.lua")
+dofile(minetest.get_modpath("new_corners").."/stone.lua")
+dofile(minetest.get_modpath("new_corners").."/stone_brick.lua")
+dofile(minetest.get_modpath("new_corners").."/wood.lua")
