3e67cd42aef2f8a140555ecdce8dd86625e729e5
Napiophelios <napiophelios@gmail.com>
2015-06-02 23:49:57 -0400

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,13 @@
+--[[
+'New Corners' mod [new_corners]
+by Napiophelios
+License: WTFPL
+This is an experiment to reproduce 'Mycorners' mod
+by Don Bateman but without adding textures.
+--]]
+
+dofile(minetest.get_modpath("new_corners").."/sandstone.lua")
+--dofile(minetest.get_modpath("new_corners").."/sandstone_brick.lua")
+dofile(minetest.get_modpath("new_corners").."/stone.lua")
+--dofile(minetest.get_modpath("new_corners").."/stone_brick.lua")
+dofile(minetest.get_modpath("new_corners").."/wood.lua")
